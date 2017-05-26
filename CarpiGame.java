7550b39bd2c252be3bbb02c17117a511300168d0
CarpiDar <gregscatko@gmail.com>
2017-05-26 10:39:46 -0400

    Add files via upload

---
+++ CarpiGame.java
@@ -0,0 +1,25 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+public class CarpiGame extends JFrame {
+	
+	public CarpiGame () {
+		initGame();
+	}
+	public void initGame() {
+		add(new Board());
+		setTitle("Carpi Game");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(350,350);
+		setLocationRelativeTo(null);
+		setResizable(false);
+	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				CarpiGame ex = new CarpiGame();
+				ex.setVisible(true);
+			}
+		});
+	}
+
+}
