1246329644593e1749b2b242a35979462a726c2b
splarpi <nikhilramesh01@gmail.com>
2017-05-30 18:29:45 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -4,6 +4,15 @@ public abstract class Entity
 {
 	public int health, speed, xPos, yPos, direction;
 	public Rectangle hitbox;
-
-	public abstract void move(boolean[] inputs);
+	
+	public Entity(int hp, int sp, int x, int y, int hbW, int hbH, int topLeft, int topRight)
+	{
+		health = hp;
+		speed = sp;
+		xPos = x;
+		yPos = y;
+		direction = 1;
+		hitbox = new Rectangle(hbW, hbH, topLeft, topRight);
+	}
+	public abstract void move();
 }
