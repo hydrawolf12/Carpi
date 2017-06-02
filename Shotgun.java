d79b61d1f674d2c18d20e7c7e7106fc86e37b868
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-02 13:48:33 -0400

    Update Shotgun.java

--- Shotgun.java
+++ Shotgun.java
@@ -3,8 +3,8 @@ public class Shotgun extends Gun
 {
   public void shoot(int direction) 
   {
-	  getBullets().add(new Bullet(player, direction, false, true, player.getDamage() * 2));
-	  getBullets().add(new Bullet(player, direction + 1, false, true, player.getDamage() * 2));
-	  getBullets().add(new Bullet(player, direction - 1, false, true, player.getDamage() * 2));
+	  getBullets().add(new Bullet(player.getxPos(), player.getyPos(), direction, false, true, player.getDamage() * 2));
+	  getBullets().add(new Bullet(player.getxPos(), player.getyPos(), direction + 1, false, true, player.getDamage() * 2));
+	  getBullets().add(new Bullet(player.getxPos(), player.getyPos(), direction - 1, false, true, player.getDamage() * 2));
   }
 }
