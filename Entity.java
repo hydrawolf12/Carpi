56cba5f3eac3b417c17d5e5d668904532595fc28
splarpi <nikhilramesh01@gmail.com>
2017-06-02 14:09:54 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -2,8 +2,8 @@ import java.awt.Rectangle;
 
 public abstract class Entity
 {
-	public int health, speed, xPos, yPos, direction;
-	public Rectangle hitbox;
+	private int health, speed, xPos, yPos, direction;
+	private Rectangle hitbox;
 	
 	public Entity(int hp, int sp, int x, int y, int hbW, int hbH, int topLeft, int topRight)
 	{
@@ -14,5 +14,27 @@ public abstract class Entity
 		direction = 1;
 		hitbox = new Rectangle(hbW, hbH, topLeft, topRight);
 	}
+	
+	public int getHealth()
+	{
+		return health;
+	}
+	public int getSpeed()
+	{
+		return speed;
+	}
+	public int getxPos()
+	{
+		return xPos;
+	}
+	public int getyPos()
+	{
+		return yPos;
+	}
+	public int getDirection()
+	{
+		return direction;
+	}
+	
 	public abstract void move();
 }
