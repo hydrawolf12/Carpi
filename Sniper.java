41bbed19f67af20cb8fea71b4f443c3eed3b6cc3
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-02 13:49:32 -0400

    Update Sniper.java

--- Sniper.java
+++ Sniper.java
@@ -3,6 +3,6 @@ public class Sniper extends Gun
 {
   public void shoot(int direction) 
   {
-	  getBullets().add(new Bullet(player, direction, false, true, player.getDamage() * 3));
+	  getBullets().add(new Bullet(player.getxPos(), player.getyPos(), direction, false, true, player.getDamage() * 3));
   }
 }
