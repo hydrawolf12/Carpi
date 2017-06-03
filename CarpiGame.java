7361affa579f9fc9308d62d0405cca1706603254
Scalko <Scalko@192.168.1.5>
2017-06-02 21:27:03 -0400

    Removed useless

--- CarpiGame.java
+++ CarpiGame.java
@@ -2,31 +2,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 public class CarpiGame extends JFrame 
 {	
-	/*public CarpiGame () Dis some Trash b0i
-	{
-		initGame();
-	}
-	public void initGame() 
-	{
-		add(new Board());
-		setTitle("Carpi Game");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(350,350);
-		setLocationRelativeTo(null);
-		setResizable(false);
-	}
-	public static void main(String[] args) 
-	{
-		EventQueue.invokeLater(new Runnable() 
-		{
-			public void run() 
-			{
-				CarpiGame ex = new CarpiGame();
-				ex.setVisible(true);
-			}
-		});
-	}*/
-	
 	public static void main(String[] args)
 	{
 		JFrame f = new JFrame("Cowboy Carpi");
