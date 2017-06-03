8b5bed7f88a5922bc92e0308f20afe844ad4572c
splarpi <nikhilramesh01@gmail.com>
2017-06-03 14:28:27 -0400

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -2,9 +2,9 @@ import java.awt.Rectangle;
 
 public class Boss extends Zombie
 {
-	public Boss(int health, int speed, int x, int y, int w, int h, int topL, int topR, Player mccree) //initiates boss based on speed position and health
+	public Boss(int health, int speed, int x, int y, int w, int h, int topX, int topY, int width, int height, Player mccree) //initiates boss based on speed position and health
 	{
-		super(health, speed, x, y, w, h, topL, topR, mccree);
+		super(health, speed, x, y, w, h, topX, topY, width, height mccree);
 	}
 	
 	public void remove(int pos, Player mccree) // removes boss from field, upgrades player, returns spawn rate to normal
