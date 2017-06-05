ff6a1e4199d6bb6f4824997e75af051bc6fb66ee
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-05 13:00:42 -0400

    Update Pistol.java

--- Pistol.java
+++ Pistol.java
@@ -1,7 +1,7 @@
 
 public class Pistol extends Gun
 {
-  public void shoot(int direction) 
+  public void shoot(int direction, Player player) 
   {
 	  getBullets().add(new Bullet(player.getxPos(), player.getyPos(), direction, false, false, player.getDamage()));
   }
