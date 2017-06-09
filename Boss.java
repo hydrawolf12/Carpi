f387d2cbf5bfaee546a765810966584c2acce163
splarpi <nikhilramesh01@gmail.com>
2017-06-09 18:23:47 -0400

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -5,9 +5,9 @@ public class Boss extends Zombie
 	private int bossKillCount = 0;
 	// private boolean isBossDefeated;
 	private Timer spawn = Board.returnZombieSpawner();
-	public Boss(int health, int speed, int x, int y, Player mccree) //initiates boss based on speed position and health
+	public Boss(int health, int speed, int x, int y, Board playBoard) //initiates boss based on speed position and health
 	{
-		super(health, speed, x, y, x - 25, y - 25, 50, 50, mccree);
+		super(health, speed, x, y, x - 25, y - 25, 50, 50, playBoard);
 		// isBossDefeated = false;
 	}
 	
@@ -17,9 +17,10 @@ public class Boss extends Zombie
 		return isBossDefeated;
 	}
 	*/
-	public void remove(int pos) // removes boss from field, upgrades player, returns spawn rate to normal
+	/*public void remove(int pos) // removes boss from field, upgrades player, returns spawn rate to normal
 	{
-		currentZombs.remove(pos);
+		Player mccree = board.getPlayer();
+		// currentZombs.remove(pos);
 		//isBossDefeated = true;
 		Spawner.incrementHealth(2);
 		Spawner.incrementSpeed(1);
@@ -36,6 +37,31 @@ public class Boss extends Zombie
 		{
 			mccree.updateFireRate(2);
 		}
+	} */
+	public boolean takeDamage(int a, int pos)
+	{
+		health -= a;
+		if (health <= 0)
+		{
+			Player mccree = board.getPlayer();
+			Spawner.incrementHealth(2);
+			Spawner.incrementSpeed(1);
+			Spawner.incrementRate(1);
+			bossKillCount++;
+			spawn.setDelay(Spawner.returnRate());
+			if (bossKillCount % 3 == 0)
+				mccree.updateHP(1); // int a subject to change
+			else if (bossKillCount % 3 == 2)
+			{
+				mccree.updateDamage(2);
+			}
+			else if (bossKillCount % 3 == 1)
+			{
+				mccree.updateFireRate(2);
+			}
+			return true;
+		} 
+		return false;
 	}
 	
 }
