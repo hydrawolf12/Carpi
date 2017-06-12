5f999fa67eb70d64a84e45626f59a5c7140ad9d1
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-12 18:54:36 -0400

    Create Shotgun.java

--- src/Shotgun.java
+++ src/Shotgun.java
@@ -3,8 +3,8 @@ public class Shotgun extends Gun
 {
   public void shoot(int direction, Board b) 
   {
-          b.getBullets().add(new Bullet(b.getPlayer.getxPos(), b.getPlayer.getyPos(), direction, false, true, b.getPlayer.getDamage() * 2, b));
-	  b.getBullets().add(new Bullet(b.getPlayer.getxPos(), b.getPlayer.getyPos(), direction + 1, false, true, b.getPlayer.getDamage() * 2, b));
-	  b.getBullets().add(new Bullet(b.getPlayer.getxPos(), b.getPlayer.getyPos(), direction - 1, false, true, b.getPlayer.getDamage() * 2, b));
+          b.getBullets().add(new Bullet(b.getPlayer().getxPos(), b.getPlayer().getyPos(), direction, false, true, b.getPlayer().getDamage() * 2, b));
+	  b.getBullets().add(new Bullet(b.getPlayer().getxPos(), b.getPlayer().getyPos(), direction + 1, false, true, b.getPlayer().getDamage() * 2, b));
+	  b.getBullets().add(new Bullet(b.getPlayer().getxPos(), b.getPlayer().getyPos(), direction - 1, false, true, b.getPlayer().getDamage() * 2, b));
   }
 }
