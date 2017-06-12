a4d2e2ed1905f13a85b37f2af57266a5fdb0ec86
CarpiDar <gregscatko@gmail.com>
2017-06-12 10:55:19 -0400

    Create Sniper.java

--- src/Sniper.java
+++ src/Sniper.java
@@ -1,7 +1,7 @@
 
 public class Sniper extends Gun
 {
-  public void shoot(int direction, Player player) 
+  public void shoot(int direction, Board b) 
   {
 	  b.getBullets().add(new Bullet(b.getPlayer.getxPos(), b.getPlayer.getyPos(), direction, false, true, b.getPlayer.getDamage() * 3, b));
   }
