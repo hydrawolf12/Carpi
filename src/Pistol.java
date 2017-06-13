d04b1ac2bcdc5f00dfd8d9dfdf3fed95f4ceedd2
CarpiDar <gregscatko@gmail.com>
2017-06-13 19:57:22 -0400

    Create Pistol.java

--- src/Pistol.java
+++ src/Pistol.java
@@ -3,7 +3,7 @@ public class Pistol extends Gun
 {
   public void shoot(int direction, Board b) 
   {
-	  b.getBullets().add(new Bullet(b.getPlayer().getxPos(), b.getPlayer().getyPos(), direction, false, false, b.getPlayer().getDamage(), b));
+	  b.getBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, false, false, b.getPlayer().getDamage(), b));
   }
   
   public int returnType()
