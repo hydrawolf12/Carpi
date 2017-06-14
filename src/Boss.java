16110a1439ad4c21fd3920cff619cf1154259010
CarpiDar <gregscatko@gmail.com>
2017-06-14 17:16:04 -0400

    Create Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -30,7 +30,7 @@ public class Boss extends Zombie
 		spawn.setDelay(Spawner.returnRate());
 		if (bossKillCount % 3 == 0)
 			mccree.updateHP(1); // int a subject to change
-		else if (bossKillCount % 3 == 2)
+		else if (bossKillCount % 3 == 2)//
 		{
 			mccree.updateDamage(2);
 		}
