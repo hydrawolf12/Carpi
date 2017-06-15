9e2ff5bdaf2ea6c90851577e810b276573620617
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-15 18:28:41 -0400

    Create Pistol.java

--- src/Pistol.java
+++ src/Pistol.java
@@ -1,6 +1,11 @@
 
 public class Pistol extends Gun
 {
+  private int type;
+  public Pistol()
+  {
+     type = 0;
+  }
   public void shoot(int direction, Board b) 
   {
 	  b.getBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, false, false, b.getPlayer().getDamage(), b));
@@ -8,6 +13,6 @@ public class Pistol extends Gun
   
   public int returnType()
   {
-          return 0;
+          return type;
   }
 }
