22ba3800b2864876f68f62f430fffbee2bb4dc8b
splarpi <nikhilramesh01@gmail.com>
2017-06-17 18:38:04 -0400

    Update Zombie.java

--- src/Zombie.java
+++ src/Zombie.java
@@ -105,38 +105,39 @@ public class Zombie extends Entity
 	    
 	    	angle = angle % 360;
 	    
-	    	if (angle < 45)
-	    	{
-	    		this.setDirection(0);
-	    	}
-	    	else if(angle < 90)
+	        
+		if (angle > 22.5 && angle <= 67.5)
 	    	{
 	    		this.setDirection(45);
 	    	}
-	    	else if(angle < 135)
+	    	else if(angle > 67.5 && angle <= 112.5)
 	    	{
 	    		this.setDirection(90);
 	    	}
-	    	else if(angle < 180)
+	    	else if(angle > 112.5 && angle <= 157.5)
 	    	{
 	    		this.setDirection(135);
 	    	}
-	    	else if(angle < 225)
+	    	else if(angle > 157.5 && angle <= 202.5)
 	    	{
 	    		this.setDirection(180);
 	    	}
-	    	else if(angle < 270)
+	    	else if(angle > 202.5 && angle <= 292.5)
 	    	{
 	    		this.setDirection(225);
 	    	}
-	    	else if(angle < 315)
+	    	else if(angle > 292.5 && angle <= 337.5)
 	    	{
 	    		this.setDirection(270);
 	    	}
-	    	else if(angle < 360)
+	    	else if(angle > 337.5 && angle <= 382.5)
 	    	{
 	    		this.setDirection(315);
 	    	}
+	    	else if (angle > 337.5 && angle <= 22.5)
+		{
+			this.setDirection(0)
+		}
 	   }
 	}
 	public void collisionDetect()
