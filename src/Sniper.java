14a2b18a7e54b0216419d1746ffbde0d1f2f5e8c
CarpiDar <gregscatko@gmail.com>
2017-06-17 19:02:08 -0400

    Update Sniper.java

--- src/Sniper.java
+++ src/Sniper.java
@@ -3,7 +3,7 @@ public class Sniper extends Gun
 {
   public void shoot(int direction, Board b) 
   {
-	  b.getBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYos(), direction, false, true, b.getPlayer().getDamage() * 3, b));
+	  b.getCurrentBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, false, true, b.getPlayer().getDamage() * 3, b));
   }
   
   public int returnType()
