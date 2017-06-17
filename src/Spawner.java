affceaeba572479ee4a2ab80140893d7adccfc28
CarpiDar <gregscatko@gmail.com>
2017-06-17 19:02:27 -0400

    Update Spawner.java

--- src/Spawner.java
+++ src/Spawner.java
@@ -2,15 +2,16 @@ public class Spawner
 {
 	private static int currentSpeed;
 	private static int currentHealth;
-	private static int rate;
+	private static double rate;
 	private Board board;
+	private double zombieT;
 	// private static Player mccree;
 	
 	public Spawner(Board a)
 	{
 		currentSpeed = 1;
 		currentHealth = 1;
-		rate = 1;
+		rate = 3;
 		// mccree = player;
 		board = a;
 	}
@@ -29,38 +30,7 @@ public class Spawner
 		rate += a;
 	}
 	
-	public static int returnRate()public class Spawner
-{
-	private static int currentSpeed;
-	private static int currentHealth;
-	private static int rate;
-	private Board board;
-	// private static Player mccree;
-	
-	public Spawner(Board a)
-	{
-		currentSpeed = 1;
-		currentHealth = 1;
-		rate = 300;
-		// mccree = player;
-		board = a;
-	}
-	public static void incrementSpeed(int a)
-	{
-		currentSpeed += a;
-	}
-	
-	public static void incrementHealth(int a)
-	{
-		currentHealth += a;
-	}
-	
-	public static void incrementRate(int a)
-	{
-		rate += a * 60;
-	}
-	
-	public static int returnRate() //IN MILLISECONDS, NOT SECONDS.  SO MULTIPLE WHATEVER IT CURRENT IT IS BY 1000.
+	public static double returnRate() //IN MILLISECONDS, NOT SECONDS.  SO MULTIPLE WHATEVER IT CURRENT IT IS BY 1000.
 	{
 		return rate;
 	}
@@ -124,71 +94,20 @@ public class Spawner
 		return zimbabwe;
 		
 	}
-
-}
- //IN MILLISECONDS, NOT SECONDS.  SO MULTIPLE WHATEVER IT CURRENT IT IS BY 1000.
-	{
-		return rate;
+	public void spawn(double delta) {
+		zombieT += delta;
+		if (zombieT >= rate) {
+			System.out.println("spawn has been called");
+			System.out.println("The killCount is " + board.returnKillCount());
+			if (board.returnKillCount() >= 100) {
+				board.getCurrentZombs().add(spawnBoss());
+				board.setKillCount(0);
+				System.out.println("A zombie boss has spawned");
+			} else {
+				board.getCurrentZombs().add(spawnZombie());
+				System.out.println("A zombie has spawned");
+			}
+			zombieT = 0;
+		}
 	}
-	public Boss spawnBoss()
-	{
-		Boss baby = new Boss(currentHealth * 5, currentSpeed * 3, board.getxEnd() / 2, 30, board);
-		baby.calcAng();
-		//board.addZomb(baby);
-		return baby;
-		
-	}
-	public Zombie spawnZombie()
-	{
-		int x = 0;
-		int y = 0;
-		int frameWidth = board.getxEnd();
-		int frameHeight = board.getyEnd();
-		int a = (int)(Math.random() * 8) + 1;
-		if (a == 1)
-		{
-			x = frameWidth / 8;
-			y = 1;
-		}
-		else if(a == 2)
-		{
-			x = frameWidth - frameWidth / 8;
-			y = 1;
-		}
-		else if(a == 3)
-		{
-			x = frameWidth;
-			y = frameHeight / 8;
-		}
-		else if(a == 4)
-		{
-			x = frameWidth;
-			y = frameHeight - frameHeight / 8;
-		}
-		else if(a == 5)
-		{
-			x = frameWidth - frameWidth / 8;
-			y = frameHeight;
-		}
-		else if(a == 6)
-		{
-			x = frameWidth / 8;
-			y = frameHeight;
-		}
-		else if(a == 7)
-		{
-			x = 1;
-			y = frameHeight - frameHeight / 8;
-		}
-		else if(a == 8)
-		{
-			x = 1;
-			y = frameHeight / 8;
-		}
-		Zombie zimbabwe = new Zombie(currentHealth, currentSpeed, x, y, board);
-		zimbabwe.calcAng();
-		return zimbabwe;
-		
-	}
-
 }
