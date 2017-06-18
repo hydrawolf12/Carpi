289e2a581d520a93d0b1fb6dd4aa4256ded65346
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-18 18:49:33 -0400

    Update Pistol.java

--- src/Pistol.java
+++ src/Pistol.java
@@ -1,6 +1,7 @@
 
 public class Pistol extends Gun
 {
+  //Generates a single non-piercing bullet
   public void shoot(int direction, Board b) 
   {
 	  b.getCurrentBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, false, false, b.getPlayer().getDamage() * 2, b));
