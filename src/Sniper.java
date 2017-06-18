4777cf50a0559b3b47da765f48bac3a8d2a76ebf
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-18 18:48:28 -0400

    Update Sniper.java

--- src/Sniper.java
+++ src/Sniper.java
@@ -1,6 +1,7 @@
 
 public class Sniper extends Gun
 {
+  //Generates a single piercing bullet
   public void shoot(int direction, Board b) 
   {
 	  b.getCurrentBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, true, false, b.getPlayer().getDamage() * 3, b));
