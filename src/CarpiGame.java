34c8fba169cf407cb766789e1cbd0ee81d1bd7fa
splarpi <nikhilramesh01@gmail.com>
2017-06-18 19:43:31 -0400

    Update CarpiGame.java

--- src/CarpiGame.java
+++ src/CarpiGame.java
@@ -1,7 +1,9 @@
-//import java.awt.EventQueue;
 import javax.swing.JFrame;
 public class CarpiGame extends JFrame 
 {
+	//runs game and names it Cowboy Carpi
+	//adds a Board object and sets the size to 1000x1000
+	//makes the board visible
 	public static void main(String[] args)
 	{
 		JFrame f = new JFrame("Cowboy Carpi");
