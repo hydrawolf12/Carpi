4b584d8e71df72da21021e36d916a6cc80be033a
splarpi <nikhilramesh01@gmail.com>
2017-06-18 19:29:34 -0400

    Update Spawner.java

--- src/Spawner.java
+++ src/Spawner.java
@@ -5,14 +5,13 @@ public class Spawner
 	private static double rate;
 	private Board board;
 	private double zombieT;
-	// private static Player mccree;
 	
+	//initializes Spawner object and sets base speed, health, and spawnrate for zombie objects
 	public Spawner(Board a)
 	{
 		currentSpeed = 1;
 		currentHealth = 1;
 		rate = 3;
-		// mccree = player;
 		board = a;
 	}
 	public static void incrementSpeed(int a)
@@ -30,17 +29,22 @@ public class Spawner
 		rate *= a;
 	}
 	
-	public static double returnRate() //IN MILLISECONDS, NOT SECONDS.  SO MULTIPLE WHATEVER IT CURRENT IT IS BY 1000.
+	public static double returnRate()
 	{
 		return rate;
 	}
+	
+	//initializes a Boss object with 5 times the health of normal zombies, at the top of the screen, and with hitbox size 70x70
+	//calls calcAng() method and returns Boss object to be added to ArrayList
 	public Boss spawnBoss()
 	{
 		Boss baby = new Boss(currentHealth * 5, currentSpeed, board.getxEnd() / 2, 30, 70, 70, board);
 		baby.calcAng();
-		//board.addZomb(baby);
 		return baby;
 	}
+	
+	//initializes Zombie at a randomly selected position
+	//calls calcAng method and returns Zombie object to be added to ArrayList
 	public Zombie spawnZombie()
 	{
 		int x = 0;
@@ -91,18 +95,25 @@ public class Spawner
 		Zombie zimbabwe = new Zombie(currentHealth, currentSpeed, x, y, 50, 50, board);
 		zimbabwe.calcAng();
 		return zimbabwe;
-		
 	}
-	public void spawn(double delta) {
+	
+	//increments timer by delta value and spawns a zombie if needed
+	//spawns a Boss if the killCount of zombies is >= 30 and resets the killCount as well as timer
+	public void spawn(double delta)
+	{
 		zombieT += delta;
-		if (zombieT >= rate) {
+		if (zombieT >= rate)
+		{
 			System.out.println("spawn has been called");
 			System.out.println("The killCount is " + board.returnKillCount());
-			if (board.returnKillCount() >= 10) {
+			if (board.returnKillCount() >= 30)
+			{
 				board.getCurrentZombs().add(spawnBoss());
 				board.setKillCount(0);
 				System.out.println("A zombie boss has spawned");
-			} else {
+			}
+			else
+			{
 				board.getCurrentZombs().add(spawnZombie());
 				System.out.println("A zombie has spawned");
 			}
