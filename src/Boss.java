d5e3b263a8f249bf8f29eda4de407dbeabc4afde
splarpi <nikhilramesh01@gmail.com>
2017-06-18 18:06:26 -0400

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,72 +1,162 @@
 import java.awt.Rectangle;
 import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 
-public class Boss extends Zombie
+public class Bullet
 {
-	private int bossKillCount = 0;
-	// private boolean isBossDefeated;
-	// private Board board;
-	public Boss(int health, int speed, int x, int y, int width, int height, Board playBoard) //initiates boss based on speed position and health
-	{
-		super(health, speed, x, y, width, height, playBoard);
-		// isBossDefeated = false;'
-	}
+    private int damage;
+    private boolean isPierce;
+    private boolean isBuckshot;
+    private int xPos;
+    private int yPos;
+    private int direction;
+    private Rectangle hitbox;
+    private double counter = 0.0;
+    private Board b;
 
+    public Bullet(int x, int y, int d, boolean p, boolean bu, int dam, Board bd)
+    {
+        b = bd;
+        yPos = y;
+        xPos = x;
+        if (d == 0 || d == 360)
+            yPos = yPos - 4;
+        else if (d == 45)
+        {
+            xPos = xPos + 4;
+            yPos = yPos - 4;
+        }
+        else if (d == 90)
+            xPos = xPos + 4;
+        else if (d == 135)
+        {
+            yPos = yPos + 4;
+            xPos = xPos + 4;
+        }
+        else if (d == 180)
+            yPos = yPos + 4;
+        else if (d == 225)
+        {
+            xPos = xPos - 4;
+            yPos = yPos + 4;
+        }
+        else if (d == 270)
+            xPos = xPos - 4;
+        else if (d == 315 || d == -45)
+        {
+            yPos = yPos - 4;
+            xPos = xPos - 4;
+        }
+        isBuckshot = bu;
+        isPierce = p;
+        damage = dam;
+        hitbox = new Rectangle(xPos - 1, yPos - 1, 3, 3);
+        direction = d;
+    }
 
-	/*public static boolean returnIsBossDefeated()
-	{
-		return isBossDefeated;
-	}
-	*/
-	/*public void remove(int pos) // removes boss from field, upgrades player, returns spawn rate to normal
-	{
-		Player mccree = board.getPlayer();
-		// currentZombs.remove(pos);
-		//isBossDefeated = true;
-		Spawner.incrementHealth(2);
-		Spawner.incrementSpeed(1);
-		Spawner.incrementRate(1);
-		bossKillCount++;
-		spawn.setDelay(Spawner.returnRate());
-		if (bossKillCount % 3 == 0)
-			mccree.updateHP(1); // int a subject to change
-		else if (bossKillCount % 3 == 2)//
-		{
-			mccree.updateDamage(2);
-		}
-		else if (bossKillCount % 3 == 1)
-		{
-			mccree.updateFireRate(2);
-		}
-	} */
-	public boolean takeDamage(int a)
-	{
-		this.setHealth(-a);
-		if (this.getHealth() <= 0)
-		{
-			Player mccree = this.returnBoard().getPlayer();
-			bossKillCount++;
-			if (bossKillCount % 3 == 0)
-			{
-				Spawner.incrementHealth(2);
-				mccree.setHealth(1); // int a subject to change
-			}
-			else if (bossKillCount % 3 == 2)
-			{
-				mccree.updateDamage(2);
-				Spawner.incrementSpeed(1);
-			}
-			else if (bossKillCount % 3 == 1)
-			{
-				mccree.updateFireRate(0.9);
-				Spawner.incrementRate(1);
-			}
-			return true;
-		}
-		return false;
-	}
-	public int returnRadius() {
-		// TODO Auto-generated method stub
-		return 35;
-	}
+    public void startTimer()
+    {
+        ActionListener action = new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent event)
+            {
+                remove();
+            }
+        };
+        Timer t = new Timer(1200, action);
+        t.setRepeats(false);
+        t.start();
+    }
+
+    public void move(double delta)
+    {
+        counter += delta;
+        if (direction == 90)
+            xPos += 10;
+        else if (direction == 45)
+        {
+            yPos -= 5;
+            xPos += 5;
+        }
+        else if (direction == 0 || direction == 360)
+            yPos -= 10;
+        else if (direction == 315 || direction == -45)
+        {
+            yPos -= 5;
+            xPos -= 5;
+        }
+        else if (direction == 270)
+            xPos -= 10;
+        else if (direction == 225)
+        {
+            yPos += 5;
+            xPos -= 5;
+        }
+        else if (direction == 180)
+            yPos += 10;
+        else if (direction == 135)
+        {
+            yPos += 5;
+            xPos += 5;
+        }
+        if(xPos >= b.getxEnd() || yPos >= b.getyEnd() || xPos <= 0 || yPos <= 0)
+        {
+            this.remove();
+        }
+        else
+        {
+            hitbox = new Rectangle(xPos - 1, yPos - 1, 3, 3);
+        }
+        if(isBuckshot && counter >= 0.35)
+        {
+            this.remove();
+        }
+    }
+
+    public void collisionDetect()
+    {
+        //ArrayList<Zombie> currentZombs = b.getCurrentZombs();
+        for (int i = 0; i < b.getCurrentZombs().size(); i++)
+        {
+            if(b.getCurrentZombs().get(i).getHitbox().intersects(this.hitbox))
+            {
+                if(b.getCurrentZombs().get(i).takeDamage(this.damage) == true)
+                {
+                    b.setKillCount(b.returnKillCount() + 1);
+                    b.getCurrentZombs().remove(i);
+                    i--;
+                }
+
+                if(isPierce == false)
+                {
+                    this.remove();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void remove()
+    {
+        ArrayList<Bullet> currentBullets = b.getCurrentBullets();
+        int index = currentBullets.indexOf(this);
+        currentBullets.remove(index);
+    }
+
+    public Bullet getBullet()
+    {
+        return this;
+    }
+    public int getXPos()
+    {
+        return xPos;
+    }
+
+    public int getYPos()
+    {
+        return yPos;
+    }
 }
