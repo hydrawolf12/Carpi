6b228d1d43378363c5f3e90c7a1ff011e2380d78
Godfather-Carpi <bobbob34324@gmail.com>
2017-06-18 18:49:05 -0400

    Update Shotgun.java

--- src/Shotgun.java
+++ src/Shotgun.java
@@ -1,6 +1,7 @@
 
 public class Shotgun extends Gun
 {
+  //Generates 3 non-piercing bullets that disappear after a limited amount of time
   public void shoot(int direction, Board b) 
   {
       b.getCurrentBullets().add(new Bullet(b.getPlayer().getXPos(), b.getPlayer().getYPos(), direction, false, true, b.getPlayer().getDamage() * 2, b));
